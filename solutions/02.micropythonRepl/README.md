712f92c7fa952151a488f8ec9612f7e2a1311fdd
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2019-06-15 13:27:04 -0400

    solution for REPL bonus problem and started next kata

---
+++ solutions/02.micropythonRepl/README.md
@@ -0,0 +1,7 @@
+This is the solution for the bonus exercise of turning LEDs on and off in a loop.
+
+Note that the code for managing the state of the red and green LEDs are tied together in a single synchronous loop.  It would be ideal to tie these states to independant events or timers.  We will explore that in a future kata.
+
+Note also that you can use CTRL-C to interrupt the program.  At that point you can interact with the REPL to turn LEDs on and off manually.
+
+This is the last coding exercise we will do without first writing tests.
---
+++ solutions/02.micropythonRepl/main.py
@@ -0,0 +1,19 @@
+# Note: ignore the error "Unable to import machine" for now.  This happens because the machine module exists 
+# only on the ESP32 chip, and not in the local programming environment (in my case, Visual Studio Code).
+# We will fix this error in a future kata.  For now, be assured (and verify) that the inport error does not 
+# occur when the code is uploaded to the ESP32
+import time
+import machine
+
+greenLed = machine.Pin(22, machine.Pin.OUT)
+redLed = machine.Pin(21, machine.Pin.OUT)
+
+while True:
+    greenLed.on()
+    time.sleep_ms(250)
+    redLed.on()
+    time.sleep_ms(250)
+    redLed.off()
+    time.sleep_ms(250)
+    greenLed.off()
+    time.sleep_ms(250)
