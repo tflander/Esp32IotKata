fe9f09d01cece120a05080c68bfdc10bdeb093d5
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2019-06-18 13:14:15 -0400

    got async code working

--- katas/picoWebSpike/main.py
+++ katas/picoWebSpike/main.py
@@ -1,5 +1,6 @@
 import network
 import socket
+import machine
 
 def wifiConnect(ssid, password):
     sta_if = network.WLAN(network.STA_IF)
@@ -8,7 +9,6 @@ def wifiConnect(ssid, password):
     print('waiting for connection...')
     while not sta_if.isconnected():
         pass
-    # print('network config:', sta_if.ifconfig())
 
     addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
     return addr, sta_if.ifconfig()
@@ -18,7 +18,6 @@ def startListener(addr):
     s = socket.socket()
     s.bind(addr)
     s.listen(5)
-    print('listening on', addr)
     return s
 
 addr, ifconfig = wifiConnect("TheForge", "speed2VALUE!")
@@ -27,10 +26,19 @@ serverSocket = startListener(addr)
 import picoweb
 app = picoweb.WebApp(__name__)
 
+count = 0
+def incrementCount(timer):
+    global count
+    count = count + 1
+
+countTimer = machine.Timer(0)  
+countTimer.init(period=1000, mode=machine.Timer.PERIODIC, callback=incrementCount)
+
+
 @app.route("/")
 def index(req, resp):
     yield from picoweb.start_response(resp)
-    yield from resp.awrite("Hello world from picoweb running on the ESP32")
+    yield from resp.awrite("Hello world from picoweb running on the ESP32. data = %s" % str(count))
 
 import logging
 log = logging.getLogger("my-logger")
