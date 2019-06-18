27ee939815476ba76e05672e65135655f0a5de65
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2019-06-18 14:42:14 -0400

    crappy code for sensing if booth is occupied

--- katas/picoWebSpike/main.py
+++ katas/picoWebSpike/main.py
@@ -1,6 +1,11 @@
 import network
 import socket
 import machine
+from hcsr04 import HCSR04
+
+EXPECTED_DISTANCE_CM = 77
+DISTANCE_TOLERANCE_CM = 10
+NUM_READS_TO_CHANGE_STATE = 3
 
 def wifiConnect(ssid, password):
     sta_if = network.WLAN(network.STA_IF)
@@ -13,6 +18,12 @@ def wifiConnect(ssid, password):
     addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
     return addr, sta_if.ifconfig()
 
+def apConnect(ssid):
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(True) 
+    ap_if.config(essid=ssid)
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+    return addr, ap_if.ifconfig()
 
 def startListener(addr):
     s = socket.socket()
@@ -20,25 +31,64 @@ def startListener(addr):
     s.listen(5)
     return s
 
-addr, ifconfig = wifiConnect("TheForge", "speed2VALUE!")
+# addr, ifconfig = wifiConnect("TheForge", "speed2VALUE!")
+addr, ifconfig = apConnect("Todd Esp32")
 serverSocket = startListener(addr)
+greenLed = machine.Pin(22, machine.Pin.OUT)
+redLed = machine.Pin(21, machine.Pin.OUT)
 
 import picoweb
 app = picoweb.WebApp(__name__)
 
-count = 0
-def incrementCount(timer):
-    global count
-    count = count + 1
+sensor = HCSR04(trigger_pin=2, echo_pin=15)
 
-countTimer = machine.Timer(0)  
-countTimer.init(period=1000, mode=machine.Timer.PERIODIC, callback=incrementCount)
+distance = -1
+isOccupied = False
+candidateStateChangeCount = 0
 
+def measureDistance(timer):
+    global distance, isOccupied, candidateStateChangeCount
+    candidateDistance = -1
+    while candidateDistance < 0 or candidateDistance > EXPECTED_DISTANCE_CM + DISTANCE_TOLERANCE_CM:
+        candidateDistance = sensor.distance_cm()
+        # print('retrying...')
+
+    distance = candidateDistance
+    candiateIsOccupied = candidateDistance < EXPECTED_DISTANCE_CM - DISTANCE_TOLERANCE_CM
+    if(candiateIsOccupied):
+        if not isOccupied:
+            if candidateStateChangeCount < NUM_READS_TO_CHANGE_STATE:
+                candidateStateChangeCount = candidateStateChangeCount + 1
+            else:
+                candidateStateChangeCount = 0
+                isOccupied = True
+    else:
+        if isOccupied:
+            if candidateStateChangeCount < NUM_READS_TO_CHANGE_STATE:
+                candidateStateChangeCount = candidateStateChangeCount + 1
+            else:
+                candidateStateChangeCount = 0
+                isOccupied = False
+
+    if(isOccupied):
+        greenLed.off()
+        redLed.on()
+    else:
+        greenLed.on()
+        redLed.off()
+
+distanceTimer = machine.Timer(0)  
+distanceTimer.init(period=1000, mode=machine.Timer.PERIODIC, callback=measureDistance)
 
 @app.route("/")
 def index(req, resp):
     yield from picoweb.start_response(resp)
-    yield from resp.awrite("Hello world from picoweb running on the ESP32. data = %s" % str(count))
+    yield from resp.awrite("'isOccupied' : " + str(isOccupied))
+
+@app.route("/debug")
+def debug(req, resp):
+    yield from picoweb.start_response(resp)
+    yield from resp.awrite("Distance = {} cm; isOccupied = {}; candidateStateChangeCount = {}".format(str(distance), isOccupied, candidateStateChangeCount))
 
 import logging
 log = logging.getLogger("my-logger")
