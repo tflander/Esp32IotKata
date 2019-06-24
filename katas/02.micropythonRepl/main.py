879e345a99d5500dcd302066ea821dd9d8827c05
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2019-06-24 11:36:41 -0400

    use emulated machine

--- katas/02.micropythonRepl/main.py
+++ katas/02.micropythonRepl/main.py
@@ -1,8 +1,10 @@
-# Note: ignore the error "Unable to import machine" for now.  This happens because the machine module exists 
-# only on the ESP32 chip, and not in the local programming environment (in my case, Visual Studio Code).
-# We will fix this error in a future kata.  For now, be assured (and verify) that the inport error does not 
-# occur when the code is uploaded to the ESP32
-import machine
+try:
+    # this works if we are running code on-chip
+    import machine
+except:
+    # this works if we are testing on a dev workstation.  
+    # if the import fails, pip install esp32-machine-emulator
+    import esp32_machine_emulator.machine as machine
 
 greenLed = machine.Pin(22, machine.Pin.OUT)
 redLed = machine.Pin(21, machine.Pin.OUT)
