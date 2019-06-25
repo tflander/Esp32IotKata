c912f75083a92b8d42b1e6cdbf50a52d79a52fba
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2019-06-25 15:42:28 -0400

    finish test driving boothInUseDetector

--- solutions/06.crappyCodeClub/boothInUseDetector.py
+++ solutions/06.crappyCodeClub/boothInUseDetector.py
@@ -2,14 +2,24 @@ from sonarBoothObjectDetector import SonarBoothObjectDetector
 
 class BoothInUseDetector:
     
-    objectDetector = None
-
     def __init__(self, trigger_pin, echo_pin, expectedDistanceCm, distanceTolerance, retriesToChangeState, sampleIntervalMs):
         self.objectDetector = SonarBoothObjectDetector(trigger_pin=trigger_pin, echo_pin=echo_pin,expectedDistanceCm=11,distanceTolerance=3)
         self.expectedDistanceCm = expectedDistanceCm
         self.distanceTolerance = distanceTolerance
         self.retriesToChangeState = retriesToChangeState
         self.sampleIntervalMs = sampleIntervalMs
+        self.currentState = False
+        self.sampleCount = 0
 
     def isAvailable(self):
-        return False
+        return self.currentState
+
+    def sample(self):
+        candidateStateChange = self.objectDetector.isObjectDetected()
+        if(candidateStateChange == self.currentState):
+            self.sampleCount = 0
+        else:
+            self.sampleCount = self.sampleCount + 1
+            if self.sampleCount >= self.retriesToChangeState:
+                self.currentState = candidateStateChange
+        return candidateStateChange
--- solutions/06.crappyCodeClub/test_boothInUseDetector.py
+++ solutions/06.crappyCodeClub/test_boothInUseDetector.py
@@ -2,43 +2,88 @@ import pytest
 from boothInUseDetector import BoothInUseDetector
 import esp32_machine_emulator.machine as machine
 
-class TestBoothInUseDetector:
-
-    TRIGGER_PIN=1
-    ECHO_PIN=2
-    EXPECTED_DISTANCE_CM=10
-    DISTANCE_TOLERANCE = 3
-    RETRIES_TO_CHANGE_STATE = 4
-    SAMPLE_INTERVAL_MS = 300
-
-    @pytest.fixture
-    def boothInUseDetector(self):
-        return BoothInUseDetector(
-            trigger_pin = self.TRIGGER_PIN, 
-            echo_pin = self.ECHO_PIN, 
-            expectedDistanceCm = self.EXPECTED_DISTANCE_CM, 
-            distanceTolerance = self.DISTANCE_TOLERANCE, 
-            retriesToChangeState = self.RETRIES_TO_CHANGE_STATE, 
-            sampleIntervalMs = self.SAMPLE_INTERVAL_MS
-        )
+class TestBoothAvailability:
 
     def test_initialStateIsOccupied(self, boothInUseDetector):
         assert not boothInUseDetector.isAvailable()
 
-    def test_boothDetectorCreatesObjectDetector(self, boothInUseDetector):
-        assert boothInUseDetector.expectedDistanceCm == self.EXPECTED_DISTANCE_CM
-        assert boothInUseDetector.distanceTolerance == self.DISTANCE_TOLERANCE
-        assert boothInUseDetector.retriesToChangeState == self.RETRIES_TO_CHANGE_STATE
-        assert boothInUseDetector.sampleIntervalMs == self.SAMPLE_INTERVAL_MS
-        assert not boothInUseDetector.objectDetector == None
-        sonar = boothInUseDetector.objectDetector.sonar
-        assert sonar.trigger.pinForTesting == self.TRIGGER_PIN
-        assert sonar.echo.pinForTesting == self.ECHO_PIN
+    def test_stateChangeWhenSonarDetectsWallExactlyEnoughTimes(self, boothInUseDetector):
+        assert not boothInUseDetector.isAvailable()
+        sampleUntilBoothAvailable(boothInUseDetector)
+        assert boothInUseDetector.isAvailable()
 
-    def test_stateDoesNotChangeWhenSonarDetectsWallExactly(self, boothInUseDetector):
-        machine.expectedPulseTimeForTesting = expectedPulsesForCm(self.EXPECTED_DISTANCE_CM)
+    def test_stateDoesNotChangeWhenInconsistantSampling(self, boothInUseDetector):
+        assert not boothInUseDetector.isAvailable()
+        inconsistantSamples = [pulseValueWhenWallFound, pulseValueWhenPersonFound, pulseValueWhenWallFound, pulseValueWhenWallFound, pulseValueWhenWallFound]
+        assert len(inconsistantSamples) == RETRIES_TO_CHANGE_STATE + 1
+        sampleAllExpected(boothInUseDetector, inconsistantSamples)
         assert not boothInUseDetector.isAvailable()
 
+    def test_stateDoesNotChangeWhenNotEnoughStateChangesSampled(self, boothInUseDetector):
+        assert not boothInUseDetector.isAvailable()
+        samples = [pulseValueWhenWallFound] * (RETRIES_TO_CHANGE_STATE - 1)
+        sampleAllExpected(boothInUseDetector, samples)
+        assert not boothInUseDetector.isAvailable()
+
+    def test_boothBecomesInUseWhenWallNolongerConsistantlyFound(self, boothInUseDetector):
+        sampleUntilBoothAvailable(boothInUseDetector)
+        assert boothInUseDetector.isAvailable()
+        samples = [pulseValueWhenPersonFound] * RETRIES_TO_CHANGE_STATE
+        sampleAllExpected(boothInUseDetector, samples)
+        assert not boothInUseDetector.isAvailable()
+
+    def test_boothDetectorCreatesObjectDetector(self, boothInUseDetector):
+        assert boothInUseDetector.expectedDistanceCm == EXPECTED_DISTANCE_CM
+        assert boothInUseDetector.distanceTolerance == DISTANCE_TOLERANCE
+        assert boothInUseDetector.retriesToChangeState == RETRIES_TO_CHANGE_STATE
+        assert boothInUseDetector.sampleIntervalMs == SAMPLE_INTERVAL_MS
+        assert not boothInUseDetector.objectDetector == None
+        sonar = boothInUseDetector.objectDetector.sonar
+        assert sonar.trigger.pinForTesting == TRIGGER_PIN
+        assert sonar.echo.pinForTesting == ECHO_PIN
+
+
+class TestSonarSampling:
+
+    def test_candidateStateIsAvailableWhenSonarDetectsWall(self, boothInUseDetector):
+        machine.expectedPulseTimeForTesting = pulseValueWhenWallFound
+        candidateAvailabilityState = boothInUseDetector.sample()
+        assert candidateAvailabilityState
+
+    def test_candidateStateIsNotAvailableWhenSonarDetectsObjectAtHighTolerance(self, boothInUseDetector):
+        machine.expectedPulseTimeForTesting = pulseValueWhenPersonFound
+        candidateAvailabilityState = boothInUseDetector.sample()
+        assert not candidateAvailabilityState
+
+def sampleAllExpected(boothInUseDetector, samples):
+    machine.expectedPulseTimeForTesting = samples
+    while len(machine.expectedPulseTimeForTesting) > 0:
+        boothInUseDetector.sample()
+
+def sampleUntilBoothAvailable(boothInUseDetector):
+    samples = [pulseValueWhenWallFound] * RETRIES_TO_CHANGE_STATE
+    sampleAllExpected(boothInUseDetector, samples)
 
 def expectedPulsesForCm(cm):
     return int(round(cm * 58.2))
+
+TRIGGER_PIN=1
+ECHO_PIN=2
+EXPECTED_DISTANCE_CM=10
+DISTANCE_TOLERANCE = 3
+RETRIES_TO_CHANGE_STATE = 4
+SAMPLE_INTERVAL_MS = 300
+
+pulseValueWhenWallFound = expectedPulsesForCm(EXPECTED_DISTANCE_CM)
+pulseValueWhenPersonFound = expectedPulsesForCm(EXPECTED_DISTANCE_CM - DISTANCE_TOLERANCE - 1)
+
+@pytest.fixture
+def boothInUseDetector():
+    return BoothInUseDetector(
+        trigger_pin = TRIGGER_PIN, 
+        echo_pin = ECHO_PIN, 
+        expectedDistanceCm = EXPECTED_DISTANCE_CM, 
+        distanceTolerance = DISTANCE_TOLERANCE, 
+        retriesToChangeState = RETRIES_TO_CHANGE_STATE, 
+        sampleIntervalMs = SAMPLE_INTERVAL_MS
+    )
