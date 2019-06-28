6352285e324e184f07174fac6f3002892385ef14
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2019-06-28 15:06:49 -0400

    crappy refactoring first pass

--- solutions/06.crappyCodeClub/README.md
+++ solutions/06.crappyCodeClub/README.md
@@ -1,7 +1,3 @@
-Let's do an exercise that is a big part of what we do as professional software engineers.  We take cryptic (crappy) code that is difficult to understand and difficult to extend, and we clean it.
+Here are my refactoring steps.  Yours might be quite different.
 
-The truth is that I was tired of MicroPython spikes and learning Python TDD, and wanted to cobble together a working solution for the phonebooth in-use detector.  Since this is a pretty simple problem, I could probably get away with releasing crappy code.  Imagine this detector was just one part of a factory automation problem.  Imagine we needed the ability to extend the code for new machines, new lines, and product change-over.  Imagine my 100 line crappy program was now 10,000 lines of crappy code.  Imagine I needed to train someone to maintain 10,000 lines of crappy code.  Imagine the plant manager expected that we were able to make changes quickly and safely.
-
-I'll put my solution in the solutions folder, but you may end up with something quite different.  Make sure you don't break the code as you clean and test protect it.
-
-I may defer test protecting the picoweb code.  We will see what happens.
+- Move the boilerplate network code to boot.py.  This allows me to focus on the real logic in main.py
--- solutions/06.crappyCodeClub/main.py
+++ solutions/06.crappyCodeClub/main.py
@@ -1,47 +1,21 @@
 try:
-    import network
-except:
-    import network_stub as network
-    
-import socket
-try:
     import machine
 except:
     import esp32_machine_emulator.machine as machine
 
 from hcsr04UltrasonicDistanceSensor import Hcsr04UltrasonicDistanceSensor
 
-EXPECTED_DISTANCE_CM = 77
-DISTANCE_TOLERANCE_CM = 10
-NUM_READS_TO_CHANGE_STATE = 3
-
-def wifiConnect(ssid, password):
-    sta_if = network.WLAN(network.STA_IF)
-    sta_if.active(True)
-    sta_if.connect(ssid, password)
-    print('waiting for connection...')
-    while not sta_if.isconnected():
-        pass
-
-    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-    return addr, sta_if.ifconfig()
-
-def apConnect(ssid):
-    ap_if = network.WLAN(network.AP_IF)
-    ap_if.active(True) 
-    ap_if.config(essid=ssid)
-    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-    return addr, ap_if.ifconfig()
-
-def startListener(addr):
-    s = socket.socket()
-    s.bind(addr)
-    s.listen(5)
-    return s
+from boot import apConnect, startListener
 
 # addr, ifconfig = wifiConnect("TheForge", "speed2VALUE!")
 addr, ifconfig = apConnect("Todd Esp32")
 serverSocket = startListener(addr)
+
+
+EXPECTED_DISTANCE_CM = 77
+DISTANCE_TOLERANCE_CM = 10
+NUM_READS_TO_CHANGE_STATE = 3
+
 greenLed = machine.Pin(22, machine.Pin.OUT)
 redLed = machine.Pin(21, machine.Pin.OUT)
 
